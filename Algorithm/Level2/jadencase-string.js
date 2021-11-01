d07a35839865415be4c76388e520e407a6e8502c
sejong77 <77sejong@gmail.com>
2021-11-01 13:13:35 +0900

    JadenCase 문자열 만들기

---
+++ Algorithm/Level2/jadencase-string.js
@@ -0,0 +1,25 @@
+/* JadenCase 문자열 만들기 */
+
+function solution(s) {
+  const strToArr = s.toLowerCase().split(' ');
+  let result = [];
+
+  for (let i = 0; i < strToArr.length; i++) {
+    let index = strToArr[i];
+
+    if (isNaN(index[0]) && index[0] !== undefined) {
+      index = index.replace(index[0], index[0].toUpperCase());
+    }
+
+    result.push(index);
+  }
+
+  let arr = ['', 'abc', 'bcd'];
+
+  console.log(isNaN(undefined));
+}
+
+// 테스트
+solution('3people unFollowed me');
+solution('for the last week');
+// solution(' a  b');
